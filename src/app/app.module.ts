bb3412adbb928f32fc71e3ad14e6e69b7b1f6296
trizmera <mllebia@live.com>
2023-07-25 19:43:21 -0300

    begin-project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { PersonalInformationComponent } from './personal-information/personal-information.component';
+import { EducationComponent } from './education/education.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    PersonalInformationComponent,
+    EducationComponent,
+    WorkExperienceComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
