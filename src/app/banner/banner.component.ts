4eba52615d8c34f4027e0dff7b6f2af32ca3a8bb
trizmera <mllebia@live.com>
2023-07-26 16:33:25 -0300

    adjust-responsive

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css']
 })
-export class BannerComponent {
+export class BannerComponent implements OnInit {
+  @HostBinding("class.pc") pcMode = false;
 
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+      this.breakpointObserver
+        .observe([Breakpoints.HandsetPortrait, Breakpoints.WebLandscape])
+        .subscribe({
+          next: (result: any) => {
+            for (let breakpoint of Object.keys(result.Breakpoints)) {
+              if(result.breakpoints[breakpoint]) {
+                if(breakpoint == Breakpoints.HandsetPortrait) {
+                  this.pcMode = false;
+                }
+
+                if(breakpoint == Breakpoints.WebLandscape) {
+                  this.pcMode = true;
+                }
+              }
+            }
+          },
+        })
+  }
 }
